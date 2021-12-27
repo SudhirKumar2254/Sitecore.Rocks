1788937d75514fff79712039c95267f98f1dd2b8
Sudhir Kumar <81869981+SudhirKumar2254@users.noreply.github.com>
2021-12-27 20:04:35 +0000

    Upgrading for compatibility with VS2022
    Upgrading for compatibility with VS2022

---
+++ src/Sitecore.Rocks.VisualStudiov17/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// © 2015-2016 Sitecore Corporation A/S. All rights reserved.
+
+using System;
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle(@"Sitecore.Rocks.VisualStudio")]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage(@"en-US")]
---
+++ src/Sitecore.Rocks.VisualStudiov17/Shell/Environment/VisualStudioOutputHost.cs
@@ -0,0 +1,109 @@
+﻿// © 2015-2016 Sitecore Corporation A/S. All rights reserved.
+
+using System;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Sitecore.Rocks.Annotations;
+using Sitecore.Rocks.Diagnostics;
+
+namespace Sitecore.Rocks.Shell.Environment
+{
+    public class VisualStudioOutputHost : OutputHost
+    {
+        private static Guid outputWindowGuid = new Guid("12062116-C721-408F-95A5-9D215F78206E");
+
+        public override void Clear()
+        {
+            var outputPane = GetOutputPane();
+            if (outputPane != null)
+            {
+                outputPane.Clear();
+            }
+        }
+
+        public override void Show()
+        {
+            var outputPane = GetOutputPane();
+            if (outputPane == null)
+            {
+                return;
+            }
+
+            var activeWindow = SitecorePackage.Instance.Dte.ActiveWindow;
+
+            // var outputWindow = SitecorePackage.Instance.Dte.Windows.Item(EnvDTE.Constants.vsWindowKindOutput);
+            var outputWindow = SitecorePackage.Instance.Dte.Windows.Item("{34E76E81-EE4A-11D0-AE2E-00A0C90FFFC3}");
+            if (outputWindow == null)
+            {
+                return;
+            }
+
+            try
+            {
+                outputWindow.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                AppHost.Output.LogException(ex);
+                return;
+            }
+
+            if (activeWindow == null)
+            {
+                return;
+            }
+
+            try
+            {
+                activeWindow.Activate();
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+            }
+        }
+
+        protected override void Write(string pane, string text)
+        {
+            Assert.ArgumentNotNull(text, nameof(text));
+
+            var outputPane = GetOutputPane(pane);
+            if (outputPane != null)
+            {
+                outputPane.OutputStringThreadSafe(text + "\r\n");
+            }
+
+            // File.AppendAllText("e:\\log.txt", text + "\r\n");
+        }
+
+        [CanBeNull]
+        private IVsOutputWindowPane GetOutputPane()
+        {
+            return GetOutputPane(@"Sitecore Rocks");
+        }
+
+        [CanBeNull]
+        private IVsOutputWindowPane GetOutputPane([NotNull] string name)
+        {
+            Debug.ArgumentNotNull(name, nameof(name));
+
+            var window = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            if (window == null)
+            {
+                return null;
+            }
+
+            IVsOutputWindowPane outputPane;
+            window.GetPane(ref outputWindowGuid, out outputPane);
+            if (outputPane != null)
+            {
+                return outputPane;
+            }
+
+            window.CreatePane(ref outputWindowGuid, name, 1, 1);
+            window.GetPane(ref outputWindowGuid, out outputPane);
+
+            return outputPane;
+        }
+    }
+}
